ef67a86795bb10deeec80f51a0982bea791bc547
Yurii Kapustin <yuriy.zieit@gmail.com>
2025-05-04 13:05:54 +0300

    navigation added

---
+++ src/app/components/navigation/navigation.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'Navigation-component',
+    standalone: true,
+    imports: [],
+    templateUrl: './navigation.component.html',
+    styleUrl: './navigation.component.sass'
+  })
+
+  export class Navigation {}
--- src/app/pages/entrance-page/entrance-page.component.ts
+++ src/app/pages/entrance-page/entrance-page.component.ts
@@ -3,11 +3,12 @@ import { Header } from '../../components/header/header.component'
 import { Footer } from '../../components/footer/footer.component'
 import { Sidebar } from '../../components/sidebar/sidebar.component'
 import { Content } from '../../components/content/content.component'
+import { Navigation } from '../../components/navigation/navigation.component'
 
 @Component({
     selector: 'entrance-page',
     standalone: true,
-    imports: [Header, Footer, Sidebar, Content],
+    imports: [Header, Footer, Sidebar, Content, Navigation],
     templateUrl: './entrance-page.component.html',
     styleUrl: './entrance-page.component.sass'
   })
